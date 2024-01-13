8031339e0018d4173f3ebc36fdc111bcf7c2982f
samkalvium <samarth.s@kalvium.community>
2024-01-13 10:48:11 +0530

    final commi

--- redx-thunk-app/src/App.jsx
+++ redx-thunk-app/src/App.jsx
@@ -7,27 +7,35 @@ import actionProvider from './redux/Action';
 function App() {
 
   const [data, setData] = useState([]);
+  const [clear, setClear] = useState(false)
 
   useEffect(() => {
     store.subscribe(() => {
       setData(store.getState().data.data);
+      setClear(true)
     });
   }, []);
 
+  const handleClearData = () => {
+    setData([]);
+    setClear(false);
+  };
 
   return (
     <>
       <button onClick={() => store.dispatch(actionProvider())} >Fetch data</button>
-      {data.map((i) => (
-        <div className='data'>
+      {data.map((i,ind) => (
+        <div className='data' key={ind}>
           <div className='flex'>
-          <h1>Name of the user:- </h1> <h3>{i.name}</h3>
+            <h1>Name of the user:- </h1> <h3>{i.name}</h3>
           </div>
           <div className='flex'>
-            <h1>E-mail Id of the user:-</h1>  <h3>{i.email}</h3>
+            <h1>E-mail Id of the user:-</h1> <h3>{i.email}</h3>
           </div>
         </div>
       ))}
+      {clear && <button onClick={handleClearData}>Clear</button>}
+
 
     </>
   )
